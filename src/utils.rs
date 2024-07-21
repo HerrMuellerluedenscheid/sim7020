35cc57965bdade4a702f02efedc4fe62d85296fd
Marius Kriegerowski <marius.kriegerowski@gmail.com>
2024-07-21 21:18:42 +0200

    cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -5,9 +5,3 @@ pub fn split_u16_to_u8(number: u16) -> [u8; 2] {
     let low_byte = (number & 0xFF) as u8;
     [high_byte, low_byte]
 }
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_line_types() {}
-}
