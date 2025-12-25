e9b1088688029c40655552498bbdc198685af9ba
Javier Mateo <javi.mateosegura@gmail.com>
2025-12-26 00:13:41 +0100

    Migrate power_saving_mode.rs

--- src/at_command/power_saving_mode.rs
+++ src/at_command/power_saving_mode.rs
@@ -1,4 +1,6 @@
-use crate::at_command::{AtRequest, AtResponse, BufferType};
+#[allow(deprecated)]
+use crate::at_command::AtResponse;
+use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 use at_commands::parser::CommandParser;
 
@@ -25,8 +27,21 @@ impl From<i32> for PowerSavingModeState {
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct GetPowerSavingMode;
 
+impl GetPowerSavingMode {
+    fn parse_state(data: &[u8]) -> Result<PowerSavingModeState, AtError> {
+        let (state,) = CommandParser::parse(data)
+            .expect_identifier(b"\r\n+CPSMS: ")
+            .expect_int_parameter()
+            .expect_identifier(b"\r\n\r\nOK")
+            .finish()?;
+        let state = PowerSavingModeState::from(state);
+
+        return Ok(state);
+    }
+}
+
 impl AtRequest for GetPowerSavingMode {
-    type Response = Result<(), AtError>;
+    type Response = PowerSavingModeState;
 
     fn get_command<'a>(&'a self, buffer: &'a mut BufferType) -> Result<&'a [u8], usize> {
         at_commands::builder::CommandBuilder::create_query(buffer, true)
@@ -34,30 +49,23 @@ impl AtRequest for GetPowerSavingMode {
             .finish()
     }
 
+    #[allow(deprecated)]
     fn parse_response(&self, data: &[u8]) -> Result<AtResponse, AtError> {
-        let (state,) = CommandParser::parse(data)
-            .expect_identifier(b"\r\n+CPSMS: ")
-            .expect_int_parameter()
-            .expect_identifier(b"\r\n\r\nOK")
-            .finish()?;
-        let state = PowerSavingModeState::from(state);
+        let state = Self::parse_state(data)?;
         Ok(AtResponse::PowerSavingMode(state))
+    }
+
+    fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
+        let state = Self::parse_state(data)?;
+        return Ok(state);
     }
 }
 
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct SetPowerSavingMode;
 
-impl AtRequest for SetPowerSavingMode {
-    type Response = Result<(), AtError>;
-
-    fn get_command<'a>(&'a self, buffer: &'a mut BufferType) -> Result<&'a [u8], usize> {
-        at_commands::builder::CommandBuilder::create_set(buffer, true)
-            .named("+CPSMS")
-            .finish()
-    }
-
-    fn parse_response(&self, data: &[u8]) -> Result<AtResponse, AtError> {
+impl SetPowerSavingMode {
+    fn parse_state(data: &[u8]) -> Result<PowerSavingModeState, AtError> {
         let (state, _context) = CommandParser::parse(data)
             .expect_identifier(b"\r\n+CPSMS: ")
             .expect_int_parameter()
@@ -65,6 +73,28 @@ impl AtRequest for SetPowerSavingMode {
             .expect_identifier(b"\r\n\r\nOK")
             .finish()?;
         let state = PowerSavingModeState::from(state);
+
+        return Ok(state);
+    }
+}
+
+impl AtRequest for SetPowerSavingMode {
+    type Response = PowerSavingModeState;
+
+    fn get_command<'a>(&'a self, buffer: &'a mut BufferType) -> Result<&'a [u8], usize> {
+        at_commands::builder::CommandBuilder::create_set(buffer, true)
+            .named("+CPSMS")
+            .finish()
+    }
+
+    #[allow(deprecated)]
+    fn parse_response(&self, data: &[u8]) -> Result<AtResponse, AtError> {
+        let state = Self::parse_state(data)?;
         Ok(AtResponse::PowerSavingMode(state))
+    }
+
+    fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
+        let state = Self::parse_state(data)?;
+        return Ok(state);
     }
 }
