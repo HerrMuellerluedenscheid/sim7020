c67fb96cdd0526af5d7f205e7d9810d1226ee078
Javier Mateo <javi.mateosegura@gmail.com>
2025-12-25 20:30:53 +0100

    Migrate at_csq.rs

--- src/at_command/at_csq.rs
+++ src/at_command/at_csq.rs
@@ -1,11 +1,33 @@
-use crate::at_command::{AtRequest, AtResponse, BufferType};
+#[allow(deprecated)]
+use crate::at_command::AtResponse;
+use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct SignalQualityReport;
 
+pub struct SignalQualityResponse {
+    pub rx_signal_strength: i32,
+    pub rx_quality: i32,
+}
+
+impl SignalQualityReport {
+    fn get_signal_response(data: &[u8]) -> Result<(i32, i32), AtError> {
+        // \r\n+CSQ: 24,0\r\n\r\nOK\r\n
+        // c.f.GSM 05.08, section 8.2.4
+        let tuple = at_commands::parser::CommandParser::parse(data)
+            .expect_identifier(b"\r\n+CSQ: ")
+            .expect_int_parameter()
+            .expect_int_parameter()
+            .expect_identifier(b"\r\n\r\nOK")
+            .finish()?;
+
+        Ok(tuple)
+    }
+}
+
 impl AtRequest for SignalQualityReport {
-    type Response = Result<(), AtError>;
+    type Response = SignalQualityResponse;
 
     fn get_command<'a>(&'a self, buffer: &'a mut BufferType) -> Result<&'a [u8], usize> {
         at_commands::builder::CommandBuilder::create_execute(buffer, true)
@@ -13,15 +35,17 @@ impl AtRequest for SignalQualityReport {
             .finish()
     }
 
+    #[allow(deprecated)]
     fn parse_response(&self, data: &[u8]) -> Result<AtResponse, AtError> {
-        // \r\n+CSQ: 24,0\r\n\r\nOK\r\n
-        // c.f.GSM 05.08, section 8.2.4
-        let (rx_signal_strength, rx_quality) = at_commands::parser::CommandParser::parse(data)
-            .expect_identifier(b"\r\n+CSQ: ")
-            .expect_int_parameter()
-            .expect_int_parameter()
-            .expect_identifier(b"\r\n\r\nOK")
-            .finish()?;
+        let (rx_signal_strength, rx_quality) = Self::get_signal_response(data)?;
         Ok(AtResponse::SignalQuality(rx_signal_strength, rx_quality))
+    }
+
+    fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
+        let (rx_signal_strength, rx_quality) = Self::get_signal_response(data)?;
+        return Ok(SignalQualityResponse {
+            rx_quality: rx_quality,
+            rx_signal_strength: rx_signal_strength,
+        });
     }
 }
