47103ed1cada9cf4e1a848c43a83c54358ad108d
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 00:12:00 +0100

    Update the code to perform parsing and implement tests in battery.rs

--- src/at_command/battery.rs
+++ src/at_command/battery.rs
@@ -1,26 +1,34 @@
+//! This module contains the structs to get the battery information
+
 #[allow(deprecated)]
 use crate::at_command::AtResponse;
 use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 use at_commands::parser::CommandParser;
 
+/// Current status of the battery
 #[allow(dead_code)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct BatteryChargeStatus {
+    /// Capacity of the battery in percent
     capacity_percent: i32,
+    /// Voltage of the battery in mV
     voltage_millivolt: i32,
 }
 
+/// Command to get the battery information
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct BatteryCharge;
 
 impl BatteryCharge {
     fn get_battery_charge_status(data: &[u8]) -> Result<BatteryChargeStatus, AtError> {
         let (capacity_percent, voltage_millivolt) = CommandParser::parse(data)
-            .expect_identifier(b"\r\n+CBC: ")
+            .trim_whitespace()
+            .expect_identifier(b"+CBC: ")
             .expect_int_parameter()
             .expect_int_parameter()
-            .expect_identifier(b"\r\n\r\nOK")
+            .trim_whitespace()
+            .expect_identifier(b"OK")
             .finish()?;
         let status = BatteryChargeStatus {
             capacity_percent,
@@ -48,5 +56,30 @@ impl AtRequest for BatteryCharge {
 
     fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
         Self::get_battery_charge_status(data)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_battery_charge_request() {
+        let mut buffer = [0u8; 512];
+
+        let data = BatteryCharge.get_command(&mut buffer).unwrap();
+
+        assert_eq!(data, b"AT+CBC\r\n");
+    }
+
+    #[test]
+    fn test_battery_charge_response() {
+        let data = b"\r\n+CBC: 10,10\r\n\r\nOK\r\n";
+
+        let response = BatteryCharge.parse_response_struct(data).unwrap();
+
+        assert_eq!(response.capacity_percent, 10);
+        assert_eq!(response.voltage_millivolt, 10);
     }
 }
