ae4473bb47db220d68d66da9953bafa4b536028a
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 17:56:07 +0100

    Add tests and document ntp.rs

--- src/at_command/ntp.rs
+++ src/at_command/ntp.rs
@@ -1,6 +1,8 @@
+/// Commands for the NTP protocol
 use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 
+/// Starts a NTP query
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct StartQueryNTP<'a> {
@@ -30,6 +32,7 @@ impl AtRequest for StartQueryNTP<'_> {
     }
 }
 
+/// Stops the NTP query
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct StopQueryNTP;
@@ -45,5 +48,67 @@ impl AtRequest for StopQueryNTP {
 
     fn parse_response_struct(&self, _data: &[u8]) -> Result<Self::Response, AtError> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn start_query_ntp_without_tzinfo() {
+        let cmd = StartQueryNTP {
+            url: "pool.ntp.org",
+            tzinfo: None,
+        };
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let bytes = cmd.get_command(&mut buffer).unwrap();
+
+        assert_eq!(bytes, b"AT+CSNTPSTART=\"pool.ntp.org\"\r\n");
+    }
+
+    #[test]
+    fn start_query_ntp_with_tzinfo() {
+        let cmd = StartQueryNTP {
+            url: "time.google.com",
+            tzinfo: Some("UTC"),
+        };
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let bytes = cmd.get_command(&mut buffer).unwrap();
+
+        assert_eq!(bytes, b"AT+CSNTPSTART=\"time.google.com\",\"UTC\"\r\n");
+    }
+
+    #[test]
+    fn start_query_ntp_parse_response_ok() {
+        let cmd = StartQueryNTP {
+            url: "pool.ntp.org",
+            tzinfo: None,
+        };
+
+        let data = b"\r\nOK\r\n";
+
+        assert!(cmd.parse_response_struct(data).is_ok());
+    }
+
+    #[test]
+    fn stop_query_ntp_get_command() {
+        let cmd = StopQueryNTP;
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let bytes = cmd.get_command(&mut buffer).unwrap();
+
+        assert_eq!(bytes, b"AT+CSNTPSTOP?\r\n");
+    }
+
+    #[test]
+    fn stop_query_ntp_parse_response_ok() {
+        let cmd = StopQueryNTP;
+
+        let data = b"\r\nOK\r\n";
+
+        assert!(cmd.parse_response_struct(data).is_ok());
     }
 }
