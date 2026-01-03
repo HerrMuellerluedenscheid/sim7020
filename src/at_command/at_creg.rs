0db5a23c379d117833890db0c15a87f67200a19b
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 12:26:04 +0100

    Add tests and improve parsing on at_creg.rs

--- src/at_command/at_creg.rs
+++ src/at_command/at_creg.rs
@@ -1,3 +1,5 @@
+//! Module to check the network registration status
+
 use crate::at_command::network_registration_status::{
     NetworkRegistrationStatus, UnsolicitedResultCodes,
 };
@@ -6,14 +8,18 @@ use crate::at_command::AtResponse;
 use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 
+/// Struct to query the network registration status
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct NetworkRegistration;
 
+/// Current status of the network registration
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct NetworkRegistrationResponse {
+    /// Current state of the unsolicited messages
     pub unsolicited_result: UnsolicitedResultCodes,
+    /// Current state of the network registration
     pub status: NetworkRegistrationStatus,
 }
 
@@ -61,7 +67,7 @@ impl AtRequest for NetworkRegistration {
     }
 }
 
-// provokes an error for testing purposes
+/// provokes an error for testing purposes
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct AtCregError;
@@ -78,5 +84,28 @@ impl AtRequest for AtCregError {
 
     fn parse_response_struct(&self, _data: &[u8]) -> Result<Self::Response, AtError> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_network_registration_command() {
+
+        let mut buffer = [0u8;512];
+        let command = NetworkRegistration.get_command(&mut buffer).unwrap();
+
+        assert_eq!(command, b"AT+CREG?\r\n");
+    }
+
+    #[test]
+    fn test_network_registration_response() {
+        let data = b"\r\n+CREG: 0,0\r\n\r\nOK\r\n";
+
+        let result = NetworkRegistration.parse_response_struct(data).unwrap();
+        assert_eq!(result.unsolicited_result, UnsolicitedResultCodes::Disabled);
+        assert_eq!(result.status, NetworkRegistrationStatus::NotRegistered)
     }
 }
