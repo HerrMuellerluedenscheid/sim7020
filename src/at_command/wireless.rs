942c97bce48dc42d7ab3fbb775dbe9b527cd4edc
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 18:18:21 +0100

    Add tests and document wireless.rs

--- src/at_command/wireless.rs
+++ src/at_command/wireless.rs
@@ -1,5 +1,7 @@
+//! Module to handle the wireless commands
 use crate::at_command::{AtRequest, BufferType};
 
+/// Command to start the wireless connection
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct StartWirelessConnection;
@@ -15,5 +17,30 @@ impl AtRequest for StartWirelessConnection {
 
     fn parse_response_struct(&self, _data: &[u8]) -> Result<Self::Response, crate::AtError> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn start_wireless_connection_command() {
+        let cmd = StartWirelessConnection;
+
+        let mut buffer = [0u8; 512];
+        let bytes = cmd.get_command(&mut buffer).unwrap();
+
+        assert_eq!(bytes, b"AT+CIICR\r\n");
+    }
+
+    #[test]
+    fn start_wireless_connection_parse_response_struct() {
+        let cmd = StartWirelessConnection;
+
+        // Typical OK response, but parser ignores content
+        let data = b"\r\nOK\r\n";
+
+        assert!(cmd.parse_response_struct(data).is_ok());
     }
 }
