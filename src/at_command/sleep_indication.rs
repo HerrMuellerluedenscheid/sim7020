a653aaad26332003dbf41e85ce9a7847f8ad0bfb
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 18:08:57 +0100

    Add tests and document sleep_indication.rs

--- src/at_command/sleep_indication.rs
+++ src/at_command/sleep_indication.rs
@@ -1,11 +1,13 @@
+//! Commands to handle the sleep indication
 #[allow(deprecated)]
 use crate::at_command::AtResponse;
 use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 use at_commands::parser::CommandParser;
 
+/// States of the sleep indication
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum SleepIndication {
     Disabled,
     Enabled,
@@ -23,6 +25,7 @@ impl From<i32> for SleepIndication {
     }
 }
 
+/// Request to get the sleep indication status
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct SleepIndicationStatus;
@@ -30,9 +33,11 @@ pub struct SleepIndicationStatus;
 impl SleepIndicationStatus {
     fn get_status(data: &[u8]) -> Result<SleepIndication, AtError> {
         let (state,) = CommandParser::parse(data)
-            .expect_identifier(b"\r\n+CPSMSTATUS: ")
+            .trim_whitespace()
+            .expect_identifier(b"+CPSMSTATUS: ")
             .expect_int_parameter()
-            .expect_identifier(b"\r\n\r\nOK")
+            .trim_whitespace()
+            .expect_identifier(b"OK")
             .finish()?;
         let state = SleepIndication::from(state);
         Ok(state)
@@ -57,5 +62,51 @@ impl AtRequest for SleepIndicationStatus {
     fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
         let state = Self::get_status(data)?;
         Ok(state)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sleep_indication_from_int() {
+        assert_eq!(SleepIndication::from(0), SleepIndication::Disabled);
+        assert_eq!(SleepIndication::from(1), SleepIndication::Enabled);
+    }
+
+    #[test]
+    fn sleep_indication_status_get_command() {
+        let cmd = SleepIndicationStatus;
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let bytes = cmd.get_command(&mut buffer).unwrap();
+
+        assert_eq!(bytes, b"AT+CPSMSTATUS?\r\n");
+    }
+
+    #[test]
+    fn sleep_indication_status_parse_disabled() {
+        let data = b"\r\n+CPSMSTATUS: 0\r\nOK\r\n";
+
+        let state = SleepIndicationStatus.parse_response_struct(data).unwrap();
+
+        assert_eq!(state, SleepIndication::Disabled);
+    }
+
+    #[test]
+    fn sleep_indication_status_parse_enabled() {
+        let data = b"\r\n+CPSMSTATUS: 1\r\nOK\r\n";
+
+        let state = SleepIndicationStatus.parse_response_struct(data).unwrap();
+
+        assert_eq!(state, SleepIndication::Enabled);
+    }
+
+    #[test]
+    #[should_panic]
+    fn sleep_indication_status_parse_invalid_state() {
+        let data = b"\r\n+CPSMSTATUS: 9\r\nOK\r\n";
+        SleepIndicationStatus.parse_response_struct(data).unwrap();
     }
 }
