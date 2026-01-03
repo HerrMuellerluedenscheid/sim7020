d29e88f1c09acdde74b7b603f9af95dbd56f9378
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 12:40:53 +0100

    Add tests and document at.rs

--- src/at_command/at.rs
+++ src/at_command/at.rs
@@ -1,3 +1,5 @@
+//! Module to handle the basic AT commands
+
 #[allow(deprecated)]
 use crate::at_command::AtResponse;
 use crate::at_command::{AtRequest, BufferType};
@@ -5,6 +7,7 @@ use crate::AtError;
 #[cfg(feature = "defmt")]
 use defmt::{error, info};
 
+/// Struct to send the AT request and verify AT is OK
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(PartialEq, Clone)]
 pub struct At;
@@ -12,6 +15,7 @@ pub struct At;
 impl At {
     fn get_command_response(data: &[u8]) -> Result<(&str, &str), AtError> {
         let tuple = at_commands::parser::CommandParser::parse(data)
+            .trim_whitespace()
             .expect_identifier(b"AT\r\r\n")
             .expect_raw_string()
             .expect_identifier(b"\r\n\r\n")
@@ -45,7 +49,25 @@ impl AtRequest for At {
     }
 
     fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
-        Self::get_command_response(data)?;
+        #[cfg(feature = "defmt")]
+        info!("parse_response {=[u8]:a}", data);
+        let (_matready, _cfun) = Self::get_command_response(data)?;
+        #[cfg(feature = "defmt")]
+        info!("matready: {} | cfun: {}", _matready, _cfun);
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_at_command_request() {
+        let mut buffer = [0u8; 512];
+
+        let data = At.get_command(&mut buffer).unwrap();
+
+        assert_eq!(data, b"AT\r\n");
     }
 }
