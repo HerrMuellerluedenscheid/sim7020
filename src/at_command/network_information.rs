518ef2aecfb63b5d8931b83837ce7c145e2930f6
Javier Mateo <javi.mateosegura@gmail.com>
2026-01-03 17:51:19 +0100

    Add tests and document network_information.rs

--- src/at_command/network_information.rs
+++ src/at_command/network_information.rs
@@ -1,11 +1,13 @@
+//! Module for the network information
 #[allow(deprecated)]
 use crate::at_command::AtResponse;
 use crate::at_command::{AtRequest, BufferType};
 use crate::AtError;
 use at_commands::parser::CommandParser;
 
+/// The formats allowed for network
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum NetworkFormat {
     LongAlphanumeric,
     ShortAlphanumeric,
@@ -24,8 +26,9 @@ impl From<i32> for NetworkFormat {
     }
 }
 
+/// The network mode
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, Debug)]
 pub enum NetworkMode {
     Automatic,
     Manual,
@@ -49,8 +52,10 @@ impl From<i32> for NetworkMode {
 #[derive(PartialEq, Clone)]
 pub struct NetworkInformation;
 
+/// Max size of the oeprator
 const OPERATOR_MAX_SIZE: usize = 16;
 
+/// Network operator definition
 pub type NetworkOperator = heapless::String<OPERATOR_MAX_SIZE>;
 
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -111,5 +116,73 @@ impl AtRequest for NetworkInformation {
     fn parse_response_struct(&self, data: &[u8]) -> Result<Self::Response, AtError> {
         let network = Self::get_network_info(data)?;
         Ok(network)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn network_format_from_int() {
+        assert_eq!(NetworkFormat::from(0), NetworkFormat::LongAlphanumeric);
+        assert_eq!(NetworkFormat::from(1), NetworkFormat::ShortAlphanumeric);
+        assert_eq!(NetworkFormat::from(2), NetworkFormat::Numeric);
+    }
+
+    #[test]
+    fn network_mode_from_int() {
+        assert_eq!(NetworkMode::from(0), NetworkMode::Automatic);
+        assert_eq!(NetworkMode::from(1), NetworkMode::Manual);
+    }
+
+    #[test]
+    fn network_information_get_command() {
+        let req = NetworkInformation;
+        let mut buffer: [u8; 512] = [0; 512];
+
+        let cmd = req.get_command(&mut buffer).unwrap();
+
+        assert_eq!(cmd, b"AT+COPS?\r\n");
+    }
+
+    #[test]
+    fn parse_network_information_full_response() {
+        let data = b"\r\n+COPS: 0,2,\"26201\",7\r\n\r\nOK";
+
+        let info = NetworkInformation.parse_response_struct(data).unwrap();
+
+        assert_eq!(info.mode, NetworkMode::Automatic);
+        assert_eq!(info.format, NetworkFormat::Numeric);
+        assert_eq!(info.operator.as_ref().map(|s| s.as_str()), Some("26201"));
+    }
+
+    #[test]
+    fn parse_network_information_without_operator() {
+        let data = b"\r\n+COPS: 0\r\n\r\nOK";
+
+        let info = NetworkInformation.parse_response_struct(data).unwrap();
+
+        assert_eq!(info.mode, NetworkMode::Automatic);
+        assert_eq!(info.format, NetworkFormat::Unknown);
+        assert!(info.operator.is_none());
+    }
+
+    #[test]
+    fn parse_network_information_with_format_only() {
+        let data = b"\r\n+COPS: 1,1\r\n\r\nOK";
+
+        let info = NetworkInformation.parse_response_struct(data).unwrap();
+
+        assert_eq!(info.mode, NetworkMode::Manual);
+        assert_eq!(info.format, NetworkFormat::ShortAlphanumeric);
+        assert!(info.operator.is_none());
+    }
+
+    #[test]
+    fn parse_network_information_invalid_response() {
+        let data = b"\r\n+COPS: ,,,\r\nERROR";
+
+        assert!(NetworkInformation.parse_response_struct(data).is_err());
     }
 }
