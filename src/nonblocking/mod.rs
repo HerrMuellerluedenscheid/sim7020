5a1097386a03842f569ad1897073d6781bc67f85
Javier Mateo <javi.mateosegura@gmail.com>
2026-03-08 15:19:09 +0100

    Fix error reading the unsolicited response

--- src/nonblocking/mod.rs
+++ src/nonblocking/mod.rs
@@ -359,7 +359,7 @@ impl<'a, T: Write, U: Read + ReadReady, PowerPin: OutputPin, DtrPin: OutputPin, 
             // Check if there are no bytes left, we exhausted the buffer, or we have a break
             if read_bytes_in_iter == 0
                 || read_bytes >= response_out.len()
-                || response_out[read_bytes - 1..read_bytes] == [b'\r', b'\n']
+                || (read_bytes >= 2 && &response_out[read_bytes - 2..read_bytes] == b"\r\n")
             {
                 return Ok(read_bytes);
             }
